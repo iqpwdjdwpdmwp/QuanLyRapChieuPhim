4da07d968ac48ab0bdc94db07442fc924b7aade9
Tran Tien Dat <ttd@gmail.com>
2023-12-21 20:36:44 +0700

    update

--- QuanLyRapChieuPhim/BLL/obj/Debug/netstandard2.0/BLL.AssemblyInfo.cs
+++ QuanLyRapChieuPhim/BLL/obj/Debug/netstandard2.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1bfaf06fd4991a284987a78c267c8f649a9a9a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
