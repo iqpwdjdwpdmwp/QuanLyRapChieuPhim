4453cce7084fe27b03a3c511c76dfc581df6e10e
Tran Tien Dat <ttd@gmail.com>
2023-12-21 20:39:50 +0700

    up

--- QuanLyRapChieuPhim/DAL/obj/Debug/netstandard2.0/DAL.AssemblyInfo.cs
+++ QuanLyRapChieuPhim/DAL/obj/Debug/netstandard2.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1bfaf06fd4991a284987a78c267c8f649a9a9a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
