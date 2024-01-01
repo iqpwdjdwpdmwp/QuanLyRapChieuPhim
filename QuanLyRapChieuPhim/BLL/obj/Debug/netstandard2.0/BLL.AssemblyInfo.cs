8e3e4cf98d14014f60765cebf77889516542b282
Tran Tien Dat <trandat2622004@gmail.com>
2024-01-01 16:45:58 +0700

    sửa lại ui

--- QuanLyRapChieuPhim/BLL/obj/Debug/netstandard2.0/BLL.AssemblyInfo.cs
+++ QuanLyRapChieuPhim/BLL/obj/Debug/netstandard2.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf0385518c3cd014dadb1e2a9c16c8b2e069c2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
